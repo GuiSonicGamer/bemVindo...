aba31d9ab77ed688607ac509e64211c725a3d451
GuiSonicGamer <126628777+GuiSonicGamer@users.noreply.github.com>
2023-06-03 16:47:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function belosFilhodeUma()
+{
+    document.getElementById("enzoGABRIEU").src="MESOLTADAQUI.webp";
+}
+function byeBelos()
+{
+    document.getElementById("enzoGABRIEU").src="ENZOGABRIELLLL.jpg";
+}
